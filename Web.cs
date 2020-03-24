c5be9785c5628dea410e6673065144165b235207
Syeda Sukaina Zehra <50675369+SukainaZehra@users.noreply.github.com>
2020-03-25 01:35:40 +0500

    Add files via upload

---
+++ Web.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    // Start is called before the first frame update
+   void Start()
+    {
+        // A correct website page.
+        StartCoroutine(GetRequest());
+
+        // A non-existing page.
+        //StartCoroutine(GetRequest("https://error.html"));
+    }
+
+    IEnumerator GetRequest()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/getdate.php"))
+        {
+            // Request and wait for the desired page.
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+}
